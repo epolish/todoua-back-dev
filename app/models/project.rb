83b638c273ce0b2cb9ae3d52d5049d7211e27dd0
Eugene <polish@i.ua>
2017-08-21 18:17:22 +0300

    Initial commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :task, -> { order(position: :asc) }, :dependent => :delete_all
+  has_many :task, -> { order(position: :asc) }, dependent: :delete_all
   validates :name, presence: true
 end
